a258bcd70e8a8f272265e88a813a02d00e83fb07
BhuvanaAtmakuri <bhuvanaatma1@gmail.com>
2025-01-18 11:23:34 +0530

    changes and updates

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const cors=require('cors');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const shopifySellerRoutes = require('./routes/shopifySellerRoutes');
 const saelorSellerRoutes = require('./routes/saelorSellerRoutes');
 const wooCommerceSellerRoutes = require('./routes/wooCommerceSellerRoutes');
+
+
+
 
 const app = express();
 
 // Connect to the database
 connectDB();
 
-
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow only the frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // Include credentials like cookies if needed
+}));
 // Middleware
 app.use(express.json());
-app.use(cors());
+
 // Routes
 app.use('/ondc/shopify', shopifySellerRoutes);
 app.use('/ondc/saelor', saelorSellerRoutes);

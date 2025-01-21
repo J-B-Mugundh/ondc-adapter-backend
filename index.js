a55236e31e66a59b448555983d94e53507b4fccc
Mugundh <mugundhjb@gmail.com>
2025-01-21 16:15:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 
